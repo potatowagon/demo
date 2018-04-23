f1b0fc4fc3a5b12ef055c1a61142849092394a48
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-04-23 23:23:07 +0800

    init

--- index.js
+++ index.js
@@ -16,13 +16,32 @@ var app = {
 };
 
 /*INSERT ACCELEROMETER HERE*/
+try {
+    let sensor = new LinearAccelerationSensor({ frequency: 60 });
+    sensor.start();
 
-
-
+    sensor.onreading = () => {
+        var event = new CustomEvent('devicemotion', {
+            detail: {
+                acceleration: {
+                    x: sensor.x,
+                    y: sensor.y,
+                    z: sensor.z
+                }
+            }
+        });
+        window.dispatchEvent(event);
+    }
+    sensor.onerror = event => console.log(event.error.name, event.error.message);
+}
+catch (e) {
+    console.log(e);
+    app.usingGenericSensor = false;
+}
 
 if (window.DeviceMotionEvent || 'LinearAccelerationSensor' in window) {
     /*ADD AN EVENT LISTENER TO WINDOW*/
-    
+    window.addEventListener('devicemotion', deviceMotionHandler, false);
 }
 else {
     console.log("Sensors not supported");
@@ -70,9 +89,6 @@ function deviceMotionHandler(eventData) {
     var accReading = "<p>x: " + mAcc.x + "</br>y: " + mAcc.y + "</br>z: " + mAcc.z + "</br> max: ( " + app.max.x + ", " + app.max.y + ", " + app.max.z + " )";
     document.getElementById('acc-readings').innerHTML = accReading;
 
-    //adjust shake threshold here
-    var shakyThreshold = 5000;
-
     //do silly stuff
     var shakyThreshold = 11000;
     console.log(app.busy);
@@ -80,7 +96,9 @@ function deviceMotionHandler(eventData) {
         if (Math.abs(mAcc.x) > shakyThreshold || Math.abs(mAcc.y) > shakyThreshold || Math.abs(mAcc.z) > shakyThreshold) {
             app.busy = true;
             /*DO SILLY STUFF*/
-
+            var scream = new Audio(randomPicker(app.audio));
+            scream.play();
+            sleep(2000);
             app.busy = false;
         }
     }
@@ -110,4 +128,7 @@ function randomPicker(array) {
 }
 
 /* INSERT SCREAM FOR BUTTON CLICK FUNCTION HERE*/ 
-
+function scream() {
+    var scream = new Audio(randomPicker(app.audio));
+    scream.play();
+}
